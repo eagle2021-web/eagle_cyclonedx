ba2b37648eb36c6179487f3e843528999888b598
eagle2021-web <chenwangying16@163.com>
2023-09-17 19:58:17 +0800

    上传和存储图片

--- eagle_mysql_connect/src/main/java/com/eagle/mysql/controller/MongoController.java
+++ eagle_mysql_connect/src/main/java/com/eagle/mysql/controller/MongoController.java
@@ -2,14 +2,33 @@ package com.eagle.mysql.controller;
 
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.gridfs.GridFSBucket;
+import com.mongodb.client.gridfs.GridFSBuckets;
+import com.mongodb.client.gridfs.GridFSDownloadStream;
+import com.mongodb.client.gridfs.GridFSFindIterable;
+import com.mongodb.client.gridfs.model.GridFSFile;
+import com.mongodb.client.gridfs.model.GridFSUploadOptions;
+import com.mongodb.client.model.Filters;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.annotations.Param;
 import org.bson.Document;
+import org.bson.types.ObjectId;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.mongodb.gridfs.GridFsResource;
+import org.springframework.http.ContentDisposition;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.SQLOutput;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/mongo")
@@ -17,12 +36,71 @@ import javax.annotation.Resource;
 public class MongoController {
     @Resource
     private MongoTemplate template;
+
     @GetMapping("/add")
-    public String add(){
+    public String add() {
         MongoCollection<Document> java = template.getCollection("java");
         FindIterable<Document> documents = java.find();
         documents.forEach(System.out::println);
         return "ok";
     }
 
+    @PostMapping("/upload")
+    public String upload(@RequestParam("file") MultipartFile file) {
+
+        try {
+            System.out.println(file.getSize());
+            log.info("contentType = {}", file.getContentType());
+            log.info("Objects.requireNonNull(file.getOriginalFilename()) = {}", Objects.requireNonNull(file.getOriginalFilename()));
+            GridFSBucket gridFSBucket = GridFSBuckets.create(template.getDb());
+            GridFSUploadOptions options = new GridFSUploadOptions()
+                    .chunkSizeBytes(1024) // 设置 chunk 大小，默认为 255KB
+                    .metadata(new Document("contentType", file.getContentType()))
+                    ; // 其他元数据信息
+
+            ObjectId fileId = gridFSBucket.uploadFromStream(Objects.requireNonNull(file.getOriginalFilename()),
+                    file.getInputStream(),
+                    options);
+            // 存储完毕后的逻辑
+            log.info("File uploaded with id: {}", fileId);
+            return "ok";
+        } catch (IOException e) {
+            log.error("Failed to upload file", e);
+            return "error";
+        }
+    }
+
+    @GetMapping("/files/getFile")
+    public ResponseEntity<org.springframework.core.io.Resource> getFile(@RequestParam("filename") String filename) throws IOException {
+        GridFSBucket gridFSBucket = GridFSBuckets.create(template.getDb());
+
+        GridFSFindIterable files = gridFSBucket.find(Filters.eq("filename", filename));
+        GridFSFile file = files.first();
+        if (file != null) {
+            log.info("file={}", file);
+            System.out.println(file.getChunkSize());
+            System.out.println(file.getObjectId());
+            GridFSDownloadStream downloadStream = gridFSBucket.openDownloadStream(file.getObjectId());
+            GridFsResource resource = new GridFsResource(file, downloadStream);
+            InputStream inputStream = resource.getInputStream();
+            // 返回文件数据
+            HttpHeaders headers = new HttpHeaders();
+            System.out.println(resource);
+            Document metadata = file.getMetadata();
+            assert metadata != null;
+            String contentType = metadata.getString("contentType");
+            System.out.println(contentType);
+            System.out.println(file.getFilename());
+            headers.setContentType(MediaType.parseMediaType(contentType));
+            headers.setContentDisposition(ContentDisposition.builder("attachment").filename(file.getFilename()).build());
+
+            return ResponseEntity.ok()
+                    .headers(headers)
+                    .body(new InputStreamResource(inputStream))
+                    ;
+        } else {
+            // 文件不存在
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
---
+++ untitled/src/main/java/Main.java
@@ -0,0 +1,26 @@
+import java.awt.Robot;
+import java.awt.event.InputEvent;
+
+public class Main {
+
+    private static Robot robot;
+
+    public static void main(String[] args) {
+        // TODO 自动生成的方法存根
+        try {
+            robot = new Robot();
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+        for(int i=0;i<6;i++){
+            robot.delay(5000);
+            robot.mouseMove(2200, 1480);
+            robot.mousePress(InputEvent.BUTTON1_MASK);
+            robot.delay(20);
+            robot.mouseRelease(InputEvent.BUTTON1_MASK);
+            for(int j=0;j<15;j++)//休息15分钟
+                robot.delay(60000);
+        }
+    }
+}
